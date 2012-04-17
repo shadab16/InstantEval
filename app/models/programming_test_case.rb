a4deb8ebe503429f416d5124fde842e3f8f280db
Shadab Ansari <shadab@geekpoint.net>
2012-04-17 07:15:07 +0530

    Added validation to normalize newlines in the ProgrammingTestCase model.

--- app/models/programming_test_case.rb
+++ app/models/programming_test_case.rb
@@ -7,4 +7,13 @@ class ProgrammingTestCase < ActiveRecord::Base
 
 	belongs_to :programming_task, inverse_of: :programming_test_cases
 
+	before_validation :normalize_newlines
+
+	def normalize_newlines
+		self.stdin.gsub!(/\r\n?/, "\n")
+		self.stdin.strip!
+		self.stdout.gsub!(/\r\n?/, "\n")
+		self.stdout.strip!
+	end
+
 end
