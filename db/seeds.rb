c3ccd018d4ad73d1c84e185d9c9d59c70cc237ec
Shadab Ansari <shadab@geekpoint.net>
2012-05-12 16:08:06 +0530

    Add the database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+StatusCode.create(name: "MEM", description: "Memory Limit Exhausted")
+StatusCode.create(name: "CPU", description: "Time Limit Exhausted")
+StatusCode.create(name: "HANG", description: "Program Hangup Detected")
+StatusCode.create(name: "SIG", description: "Process Killed by Signal")
+StatusCode.create(name: "OK", description: "Successful Submission")
+StatusCode.create(name: "EXIT", description: "Non-Zero Exit Code")
+StatusCode.create(name: "COMPILE", description: "Compilation Error")
+StatusCode.create(name: "FAIL", description: "Wrong Answer")
+
+ProgrammingLanguage.create(name: "C", available: true)
+ProgrammingLanguage.create(name: "C++", available: true)
+
+user = User.new(email: "test@example.com", password: "qwerty")
+user.roles = ["admin"]
+user.save
+
