7a9bfd14934fa4c006ee1173534f6eb68bef8a26
Shadab Ansari <shadab@geekpoint.net>
2013-05-03 08:46:48 +0530

    Add a leaderboard list on homepage

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,7 +2,8 @@ class PageController < ApplicationController
 
 	def home
 		@languages = ProgrammingLanguage.all
-		@tasks = ProgrammingTask.order('created_at DESC').limit(10)
+		@tasks = ProgrammingTask.order('created_at DESC').limit(5)
+		@leaders = User.joins(:programs).group(:user_id).select('users.*, COUNT(*) as submissions').limit(5)
 	end
 
 	def about
