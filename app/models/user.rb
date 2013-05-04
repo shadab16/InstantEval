177103d762a5dc4b3791b6b5a71d4c15068145b8
Shadab Ansari <shadab@geekpoint.net>
2013-05-04 05:54:37 +0530

    Allow login via email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,14 @@ class User < ActiveRecord::Base
 	# Other available modules are: :token_authenticatable, :encryptable,
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 
-	attr_accessible :email, :password, :password_confirmation, :remember_me
+	attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+
+	# Virtual attribute for authenticating the user by username or email
+
+	attr_accessor :login
+	attr_accessible :login
+
+	validates :username, presence: true, uniqueness: {case_sensitive: false}
 
 	has_many :programs, readonly: true
 	has_many :programming_tasks, readonly: true
@@ -31,6 +38,18 @@ class User < ActiveRecord::Base
 
 	def assign_default_role
 		self.roles = %w[member] if self.roles.blank?
+	end
+
+	def self.find_first_by_auth_conditions(warden_conditions)
+		conditions = warden_conditions.dup
+		login = conditions.delete(:login)
+		if login
+			where(conditions).where(
+				['lower(username) = :value OR lower(email) = :value', {value: login.downcase}]
+			).first
+		else
+			where(conditions).first
+		end
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130503004622) do
+ActiveRecord::Schema.define(:version => 20130504000300) do
 
   create_table "program_results", :force => true do |t|
     t.integer  "program_id"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(:version => 20130503004622) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.integer  "roles_mask"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
